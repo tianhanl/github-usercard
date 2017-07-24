3aebae23ada033b63164daf7639ed0faf5eb04a7
Tianhang Liu <tianhanl@usc.edu>
2017-07-24 20:07:26 +0800

    add modifications to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,29 @@
 <template>
-  <div id="g-usercard">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
-      </li>
-      <li>
-        <a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
-      </li>
-      <li>
-        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
-      </li>
-      <li>
-        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
-      </li>
-    </ul>
+  <div id="app">
+    <h1>Demo for UserCard for GitHub</h1>
+    <input type="text" v-model="username" placeholder="Please input your username">
+    <button @click="update">update</button>
+    <usercard :username="cardUsername"></usercard>
   </div>
 </template>
 
 <script>
+import usercard from './components/UserCard.vue';
 export default {
   name: 'app',
+  components: {
+    usercard
+  },
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      username: '',
+      cardUsername: ''
+    }
+  },
+  methods: {
+    update: function () {
+      this.cardUsername = this.username;
     }
   }
 }
